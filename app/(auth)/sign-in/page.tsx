cf87bd7a8a583eeae5a9853d78f07401429f9846
Utkarsh Awasthi <utkarshawasthi100@gmail.com>
2025-07-09 12:42:29 +0530

    Set up the Authorization

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,49 @@
+"use client"
+
+import { Button } from '@/components/ui/button'
+import { GetAuthUserData } from '@/services/GlobalApi';
+import { useGoogleLogin } from '@react-oauth/google';
+import axios from 'axios';
+import Image from 'next/image'
+import React from 'react'
+
+function SignIn() {
+
+
+const googleLogin = useGoogleLogin({
+  onSuccess: async (tokenResponse) => {
+    console.log(tokenResponse);
+    if(typeof window !== undefined){
+      localStorage.setItem('user_token' , tokenResponse.access_token);
+    }
+    
+    const user = GetAuthUserData(tokenResponse.access_token);
+    console.log(user);
+  },
+  onError: errorResponse => console.log(errorResponse),
+});
+
+  return (
+    <div className = 'flex flex-col items-center justify-center h-screen gap-10'>
+      <div className = 'flex flex-col gap-10 items-center justify-center p-10 border border-gray-200 rounded-4xl shadow-2xl'>
+        <Image 
+          src = {'/logo.svg'}
+          alt = 'logo'
+          width = {100}
+          height = {100}   
+        />
+        <div className = 'font-semibold tracking-wide text-gray-700 text-2xl'>
+          Sign In To Perosnal AI Assistant & Agent
+        </div>
+        <Button 
+          className = 'p-6 cursor-pointer'
+          onClick = {() => googleLogin()}
+        >
+          Sign In With GMail
+        </Button>
+      </div>
+    </div>
+  )
+}
+
+export default SignIn
---
+++ app/provider.tsx
@@ -0,0 +1,28 @@
+"use client"
+
+import React from "react";
+import { ThemeProvider as NextThemesProvider } from "next-themes"
+import { GoogleOAuthProvider } from "@react-oauth/google";
+
+const Provider = ({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) => {
+  return (
+    <GoogleOAuthProvider clientId = {process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!}>
+      <NextThemesProvider
+        attribute="class"
+        defaultTheme="light"
+        enableSystem
+        disableTransitionOnChange
+      >
+        <div>
+          {children}
+        </div>
+      </NextThemesProvider>
+    </GoogleOAuthProvider>
+  )
+}
+
+export default Provider
