369b8941275fdaa260abcb25afc4b98a6ad0326f
Utkarsh Awasthi <utkarshawasthi100@gmail.com>
2025-07-12 00:17:58 +0530

    Chat UI Completed

--- app/(main)/workspace/_components/ChatUI.tsx
+++ app/(main)/workspace/_components/ChatUI.tsx
@@ -4,17 +4,27 @@ import { Button } from '@/components/ui/button'
 import EmptyChatState from './EmptyChatState'
 import { Send } from 'lucide-react'
 import { Input } from '@/components/ui/input'
+import { Textarea } from '@/components/ui/textarea'
+import { useState } from 'react'
 
 function ChatUI() {
+
+  const [input , setInput] = useState<string>();
+
+  const handleOnSendMessage = () => {
+    
+  }
 
   return (
     <div className = 'mt-20 p-6'>
       <EmptyChatState/>
-      <div className = 'flex justify-between p-5 gap-5 bg-slate-100 rounded-3xl h-[100px] items-center'>
-        <Input
-          className = "border-none focus-visible:ring-0 focus-visible:ring-offset-0 shadow-none"
+      <div className = 'flex justify-between p-5 gap-5 bg-slate-100 rounded-3xl h-[100px] items-center mt-20'>
+        <Textarea
+          className = "border-none focus-visible:ring-0 focus-visible:ring-offset-0 shadow-none !text-lg resize-none"
           placeholder = 'Ask anything'
-        />
+          onChange = {(e) => setInput(e.target.value)}
+          onKeyPress = {(e) => {e.key == 'Enter' && handleOnSendMessage}}
+        /> 
         <Button className = 'rounded-full cursor-pointer'>
           <Send/>
         </Button>
