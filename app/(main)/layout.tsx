9fbcae17d2b2153615a15076fb971acfb7215147
Utkarsh Awasthi <utkarshawasthi100@gmail.com>
2025-07-21 10:29:32 +0530

    updated the Header logic

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,16 +1,23 @@
+"use client"
+
 import React from 'react'
 import Header from './_components/Header';
 import Provider from './provider';
+import { usePathname } from 'next/navigation';
 
 function WorkspaceLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+   
+  const pathName = usePathname();
+  const showHeader = !pathName.startsWith('/workspace');
+
   return (
     <Provider>
       <div>
-        <Header />
+        {showHeader && <Header />}
         {children}
       </div>
     </Provider>
