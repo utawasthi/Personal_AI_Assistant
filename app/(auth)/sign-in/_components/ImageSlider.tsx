b3defd0c7703444178c5bf82842a2aa5113169bc
Utkarsh Awasthi <utkarshawasthi100@gmail.com>
2025-07-24 01:18:36 +0530

    Image SliderCompleted

--- app/(auth)/sign-in/_components/ImageSlider.tsx
+++ app/(auth)/sign-in/_components/ImageSlider.tsx
@@ -7,17 +7,25 @@ import { RiCheckboxBlankCircleFill } from "react-icons/ri";
 function ImageSlider () {
 
   const [activeSlide , setActiveSlide] = useState<number>(0);
+  const [sliderMessage , setSliderMessage] = useState<string>(images[0].content);
 
   const handeClickBackward = () => {
     setActiveSlide((activeSlide - 1 + 5) % 5);
+    setSliderMessage(images[(activeSlide - 1 + 5) % 5].content);
   }
 
   const handeClickForward = () => {
     setActiveSlide((activeSlide + 1) % 5);
+    setSliderMessage(images[(activeSlide + 1) % 5].content);
   }
 
   return (
     <div className = 'w-[100vw] h-screen lg:w-full flex flex-col items-center justify-center'>
+      <h1
+       className = 'text-2xl font-light dark:text-cyan-500 font-serif tracking-wide'
+      >
+        {sliderMessage}
+      </h1>
       <div className = 'flex items-center justify-center gap-5'>
         <div 
           className = 'text-5xl text-cyan-400 cursor-pointer'
@@ -25,26 +33,28 @@ function ImageSlider () {
         >
           <IoIosArrowBack />
         </div>
-        {
-          images.map((item , ind) => (
-            <div 
-              className = ''
-              key = {item.id}
-            >
-              {
-                activeSlide === ind ? 
-                <Image
-                  src = {item.url}
-                  alt = {item.url}
-                  height = {450}
-                  width = {500}
-                  className = 'h-[450px] w-[550px] object-cover'
-                  />
-                : null
-              }
-            </div>
-          ))
-        }
+        <div>
+          {
+            images.map((item , ind) => (
+              <div 
+                className = ''
+                key = {item.id}
+              >
+                {
+                  activeSlide === ind ? 
+                  <Image
+                    src = {item.url}
+                    alt = {item.url}
+                    height = {450}
+                    width = {500}
+                    className = 'h-[450px] w-[550px] object-contain'
+                    />
+                  : null
+                }
+              </div>
+            ))
+          }
+        </div>
         <div 
           className = 'text-5xl text-cyan-400 cursor-pointer'
           onClick = {handeClickForward}
--- services/ImageSlider.ts
+++ services/ImageSlider.ts
@@ -1,23 +1,32 @@
 export interface ImgType {
   id : number;
+  content : string;
   url : string;
 }
 
 export const images : ImgType[] = [
   {
     id : 1,
-    url : '/anthropic.png',
+    content : 'Nice combo of AI Models , just for you !',
+    url : '/slide1.png',
   },
   {
     id : 2,
-    url : '/finance.avif',
+    content : 'Emma is here for your Grammar Fixer !',
+    url : '/slide2.png',
   },
   {
     id : 3,
-    url : '/grammer-fixer.jpg'
+    content : 'Olivia is here for handling your Emails',
+    url : '/slide3.png'
   },
   {
     id : 4,
-    url : '/code-writer.jpg'
+    content : 'William lits up your Finance Management !',
+    url : '/slide4.png'
+  },{
+    id : 5,
+    content : 'Harry fires up your coding!',
+    url : '/slide5.png',
   }
 ]
